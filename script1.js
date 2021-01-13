e33a5e0d4d5916652106633d011b0e3e23b4d54b
bhorlarh <bhorlarinwah@gmail.com>
2021-01-14 00:56:20 +0100

    :zap: 3 sections done

--- script1.js
+++ script1.js
@@ -9,4 +9,48 @@ const toggleMenu = () => {
 hamburgerButton.addEventListener('click', toggleMenu);
 closeMenu.addEventListener('click', toggleMenu);
 
-// toggleMenu();
+const testimonialPaginationButtons = document.querySelectorAll(
+	'.testimonial-pagination-item'
+);
+
+// change active testimonial pagination button
+const setActivePaginationButton = (selectedButtonIndex) => {
+	testimonialPaginationButtons.forEach((button, index) => {
+		if (index !== selectedButtonIndex) {
+			button.classList.remove('active-tab');
+		} else {
+			button.classList.add('active-tab');
+		}
+	});
+};
+
+// change current pair of visible testimonials
+const displayTestimonials = (buttonIndex) => {
+	// determine which group testimonials to make visible
+	// based on the button's index
+	const testionalsToDisplay = {
+		0: [0, 1],
+		1: [2, 3],
+		2: [4, 5],
+	}[buttonIndex];
+
+	const testimonials = document.querySelectorAll('.testimonial-container');
+
+	testimonials.forEach((testimonial, index) => {
+		if (
+			testionalsToDisplay.includes(index) &&
+			!testimonial.classList.contains('visible-testimonial')
+		) {
+			testimonial.classList.add('visible-testimonial');
+		} else if (!testionalsToDisplay.includes(index)) {
+			testimonial.classList.remove('visible-testimonial');
+		}
+	});
+};
+
+testimonialPaginationButtons.forEach((button, buttonIndex) =>
+	button.addEventListener('click', () => {
+		setActivePaginationButton(buttonIndex);
+		displayTestimonials(buttonIndex);
+	})
+);
