85655793326d3bcd579de60bacba535488c2df2a
bhorlarh <bhorlarinwah@gmail.com>
2021-01-16 03:34:29 +0100

    changed css to minified css

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,8 @@ const cssnano = require('cssnano');
 module.exports = {
 	plugins: [
 		require('tailwindcss'),
-		process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-		process.env.NODE_ENV === 'production'
-			? cssnano({ preset: 'default' })
-			: null,
+		require('autoprefixer'),
+		cssnano({ preset: 'default' }),
 		purgecss({
 			content: ['./*.html'],
 			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
